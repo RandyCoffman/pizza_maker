9c5f43920ae8f70f8c0d8e9838a861ff13d0525e
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-13 15:05:49 -0500

    I added costs and made a total cost as well

--- random_pizzas.rb
+++ random_pizzas.rb
@@ -6,13 +6,18 @@ sauces = ["alfredo", "bbq", "tomato", "marinara"]
 extra = ["double pepperoni", "extra cheese", "double chicken"]
 
 
-puts "How many pizzas do you want?"
+puts "How many pizzas do you want? Pizzas are 11.70 each"
 pizzas = gets.chomp.to_i 
 
 puts "Okay, I will make you #{pizzas} pizzas"
 
 pizza = 1
 while pizza <= pizzas do
+	p "pizza"
 	p "Your crust will be #{crust.sample}, meat is #{meats.sample}, veggies are #{veggies.sample}, special toppings are #{special_toppings.sample}, sauce is #{sauces.sample}, and #{extra.sample}." 
 	pizza += 1
 end
+
+total_cost = 11.70 * pizzas 
+
+puts "The total cost will be $#{total_cost}"
