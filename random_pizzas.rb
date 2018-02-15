9f7e0168c2879d98b6766d3e6652e79170bd2be8
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-15 14:27:48 -0500

    I refactored my code to look better

--- random_pizzas.rb
+++ random_pizzas.rb
@@ -1,96 +1,149 @@
-crust = ["pan crust", "thin crust", "stuffed crust", "bacon stuffed crust"]
-meats = ["chicken", "pepperoni", "sausage", "ham", "bacon"]
-veggies = ["tomato", "pineapple", "spinach"]
-special_toppings = ["bbq sauce", "buffalo chicken"]
-sauces = ["alfredo", "bbq", "tomato", "marinara"]
-extra = ["double pepperoni", "extra cheese", "double chicken"]
-size = ["small", "medium", "large", "XL"]
+def size
+	["small", "medium", "large", "XL"]
+end
 
-puts "How many pizzas do you want?"
+def crust
+	["pan crust", "thin crust", "stuffed crust", "bacon stuffed crust"]
+end
+
+def meats
+	["chicken", "pepperoni", "sausage", "ham", "bacon"]
+end
+
+def veggies
+	["tomato", "pineapple", "spinach"]
+end
+
+def special_toppings
+	["bbq sauce", "buffalo chicken"]
+end
+
+def sauces
+	["alfredo", "bbq", "tomato", "marinara"]
+end
+
+def extra_toppings
+	["double pepperoni", "extra cheese", "double chicken"]
+end
+
+def size_cost_calc(size)
+	if size == "small"
+		return 5
+	elsif size == "medium"
+		return 7
+	elsif size == "large"
+		return 10
+	else 
+		return 13
+	end
+end
+
+def crust_cost_calc(crust)
+	if crust == "pan crust"
+		return 0
+	elsif crust == "thin crust"
+		return 0
+	elsif crust == "stuffed crust"
+		return 2
+	else 
+		return 3
+	end
+end
+
+def meats_cost_calc(meats)
+	if meats == "chicken"
+		return 3
+	elsif meats == "pepperoni"
+		return 1
+	elsif meats == "sausage"
+		return 1
+	elsif meats == "ham"
+		return 2
+	else
+		return 3
+	end
+end
+
+def veggies_cost_calc(veggies)
+	if veggies == "tomato"
+		return 1
+	elsif veggies == "pineapple"
+		return 3
+	else 
+		return 1
+	end
+end
+
+def special_top_cost_calc(special_toppings)
+	if special_toppings == "bbq sauce"
+		return 2
+	else 
+		return 4
+	end
+end
+
+def sauce_cost_calc(sauces)
+	if sauces == "alfredo"
+		return 3
+	elsif sauces == "bbq"
+		return 2
+	else 
+		0
+	end
+end
+
+def extra_top_cost_calc(extra_toppings)
+	if extra_toppings == "double pepperoni"
+		return 2
+	elsif extra_toppings == "extra cheese"
+		return 1
+	else 
+		return 3
+	end
+end
+
+cost = 0.0
+
+puts "How many pizzas would you like to order?"
 pizzas = gets.chomp.to_i 
 
 puts "Okay, I will make you #{pizzas} pizzas"
 
-pizza_cost = 0.0
-
-pizza = 1
-while pizza <= pizzas do
+for pizza in 1..pizzas 
 	pizza_size = size.sample
-	meat_cost = meats.sample
-	veggies_cost = veggies.sample
-	special_cost = special_toppings.sample
-	sauce_cost = sauces.sample
-	extra_cost = extra.sample
-	p pizza
-	p "Your pizza will be #{pizza_size}, crust will be #{crust.sample}, meat is #{meats.sample}, veggies are #{veggies.sample}, special toppings are #{special_toppings.sample}, sauce is #{sauces.sample}, and #{extra.sample}." 
-	if pizza_size == "small"
-		pizza_cost += 5.0
-	elsif pizza_size == "medium"
-		pizza_cost += 8.0
-	elsif pizza_size == "large"
-		pizza_cost += 10.0
-	else pizza_size == "XL"
-		pizza_cost += 13.0
-	end
-	if meat_cost == "chicken"
-		pizza_cost += 3.0 
-	elsif meat_cost == "pepperoni"
-		pizza_cost += 1.0
-	elsif meat_cost == "sausage"
-		pizza_cost += 1.0
-	elsif meat_cost == "ham"
-		pizza_cost += 2.0
-	else meat_cost == "bacon"
-		pizza_cost += 2.0
-	end
-	if veggies_cost == "tomato"
-		pizza_cost += 1.0 
-	elsif veggies_cost == "pineapple"
-		pizza_cost += 2.0
-	else veggies_cost == "spinach"
-		pizza_cost += 1.0
-	end
-	if special_cost == "buffalo chicken"
-		pizza_cost += 2.0
-	else
-		pizza_cost += 1
-	end
-	if sauce_cost == "alfredo"
-		pizza_cost += 0.0 
-	else
-		pizza_cost += 0
-	end
-	if extra_cost == "double pepperoni" 
-		pizza_cost += 2.0
-	elsif sauce_cost == "extra cheese"
-		pizza_cost += 1.0
-	else
-		pizza_cost += 3.0
-	end
-	p pizza_cost
-	pizza += 1
+	crust_type = crust.sample
+	meat_type = meats.sample
+	veggie_type = veggies.sample
+	sauce_type = sauces.sample
+	extra_top_type = extra_toppings.sample
+	spec_top_type = special_toppings.sample
+	puts "This is pizza ##{pizza}."
+	puts "#{pizza_size}, #{crust_type}, #{meat_type}, #{veggie_type}, #{sauce_type}, #{spec_top_type}, #{extra_top_type}"
+	bill = cost + crust_cost_calc(crust_type) + sauce_cost_calc(sauce_type) + veggies_cost_calc(veggie_type) + meats_cost_calc(meat_type) + size_cost_calc(pizza_size) + extra_top_cost_calc(extra_top_type) + special_top_cost_calc(spec_top_type)
+	puts bill
 end
-
-current_cost = pizza_cost
 
 puts "Would you like this to be delivered for an extra 5 dollars? Yes or no"
 deliver = gets.chomp
 
 if deliver == "yes"
 	fee = 5.0
-else deliver == "no"
+elsif deliver == "no"
 	fee = 0.0
+else
+	puts "I don't understand, try again"
+	return deliver
 end
 
-puts "The current cost is #{current_cost}"
+puts "The current cost is #{bill}"
 puts "What percentage would you like to tip?"
 tip_input = gets.chomp.to_f
 
 tip_percent = tip_input / 100
-tip = current_cost * tip_percent
-cost = fee + current_cost + tip
+tip = bill * tip_percent
+total = fee + bill + tip
 
-puts "The total cost will be $#{cost}"
+puts "The total cost will be $#{total}"
 puts "Your tip was $#{tip}"
 puts "Your delivery fee was $#{fee}"
-puts "Then the orignal cost was $#{current_cost}"
+puts "Then the orignal cost was $#{bill}"
